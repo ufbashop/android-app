e1ba893f1758a26ea3d0342c913e7ef4f761b8fe
Fredson Sumi <fredsonmenezessumi@gmail.com>
2019-05-01 13:21:37 -0300

    shop equals

--- app/src/main/java/br/ufba/ufbashop/objects/Shop.kt
+++ app/src/main/java/br/ufba/ufbashop/objects/Shop.kt
@@ -3,7 +3,9 @@ package br.ufba.ufbashop.objects
 data class Shop (
     val mainSellingLocation: String,
     val score: Double,
-    val ratings: Array<ShopRating>
+    val ratings: Array<ShopRating>,
+    val products: Array<Procuct>,
+    val sellers: Array<Seller>
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -14,6 +16,8 @@ data class Shop (
         if (mainSellingLocation != other.mainSellingLocation) return false
         if (score != other.score) return false
         if (!ratings.contentEquals(other.ratings)) return false
+        if (!products.contentEquals(other.products)) return false
+        if (!sellers.contentEquals(other.sellers)) return false
 
         return true
     }
@@ -22,6 +26,8 @@ data class Shop (
         var result = mainSellingLocation.hashCode()
         result = 31 * result + score.hashCode()
         result = 31 * result + ratings.contentHashCode()
+        result = 31 * result + products.contentHashCode()
+        result = 31 * result + sellers.contentHashCode()
         return result
     }
 }
