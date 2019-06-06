fcdc36de2b1bc4907273af95b8e60c23f2fb9ccc
Fredson Sumi <fredsonmenezessumi@gmail.com>
2019-06-06 01:35:32 -0300

    Criação do layout form para criação de lojas

--- app/src/main/java/br/ufba/ufbashop/objects/Shop.kt
+++ app/src/main/java/br/ufba/ufbashop/objects/Shop.kt
@@ -1,16 +1,16 @@
 package br.ufba.ufbashop.objects
 
 data class Shop (
-    val name: String,
-    val logoImage: String?,
-    val mainSellingLocation: String,
+    val name: String,//
+    val logoImage: String?,//
+    val mainSellingLocation: String,//
     val score: Double,
-    val contact: String,
-    val starts: String,
-    val ends: String,
-    val campus: String,
+    val starts: String,//
+    val ends: String,//
+    val campus: String,//
     val ratings: List<ShopRating>,
     val products: List<Product>,
-    val uuid: String = "",
-    val tags: List<String>
+    val phoneNumber: String?,//
+    val shopDescription: String,
+    val uuid: String = ""
 )
